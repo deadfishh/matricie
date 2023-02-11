3f5e64766d53cd91609d18208cf5cde44dc86258
deadfish <rachel.tjarksen@gmail.com>
2023-02-11 11:40:58 -0500

    added rref

--- Matricie.py
+++ Matricie.py
@@ -115,3 +115,51 @@ class Matricie:
         answer = self.scalar(adj, scale)
         print(answer)
         
+    
+    def row_scalar(self, mat, row, scal):
+        for i in range (len(mat)):
+            if i == row:
+                replace = self.list_scal(mat[i], scal)
+                mat[i] = replace
+                break
+        print(mat)
+        return mat
+
+    def row_add(self, mat, row, to_add, fact):
+        mat[row] = self.list_add(mat[row], mat[to_add], fact)
+        return mat
+
+    def list_scal(self, list, scal):
+        for i in range (len(list)):
+            list[i] = round(list[i] * scal, 2)
+        return list
+
+    def list_add(self, list1, list2, fact):
+        for i in range (len(list1)):
+            list1[i] = list1[i] + round((list2[i] * fact), 2)
+        print(fact)
+        return list1
+
+    def rref(self, mat):
+        if len(mat) >= len(mat[0]):
+            print("F")
+            return
+        # make lower triangular
+        for i in range (len(mat)):
+            for j in range (len(mat)):
+                print(str(i) + " " + str(j))
+                if i < j:
+                    fact = mat[j][i] * -1
+                    mat = self.row_add(mat, j, i, fact)
+                elif i == j:
+                    fact = (1/mat[j][i])
+                    mat = self.row_scalar(mat, j, fact)
+
+        for i in range (len(mat)):
+            for j in range (len(mat)):
+                if i > j:
+                    fact = mat[j][i] * -1
+                    mat = self.row_add(mat, j, i, fact)
+        
+        for i in range (len(mat)):
+            print(mat[i])
